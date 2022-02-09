e2013c9437edad333f968be907ed54da53a2d56a
Julien KARMANN <78484043+jkmn-eu@users.noreply.github.com>
2022-02-09 13:20:17 +0100

    started migrations to add foreign keys

--- db/migrate/20220203140727_add_foreign_keys.rb
+++ db/migrate/20220203140727_add_foreign_keys.rb
@@ -1,7 +1,7 @@
-class AddForeignKeysAndUserIdToPolls < ActiveRecord::Migration[6.0]
+class AddForeignKeys < ActiveRecord::Migration[6.0]
   def change
     # adds reference to users in polls & reference to polls in questions
-    add_column :user_id, :polls
+    add_column :user_id, :polls, :string
     add_foreign_key :polls, :users
     add_foreign_key :questions, :polls
   end
